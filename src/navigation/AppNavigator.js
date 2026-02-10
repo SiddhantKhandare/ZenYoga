a3a2d96c09e09ec1f9afa969d2769f7b30fc8f2e
SiddhantKhandare <sidraj.sk@gmail.com>
2026-02-10 21:09:00 +0530

    HomePage Updated

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,3 +1,26 @@
+// import React from 'react';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import SplashScreen from '../screens/SplashScreen';
+// import HomeScreen from '../screens/HomeScreen';
+// import PoseDetailScreen from '../screens/PoseDetailScreen';
+
+// const Stack = createStackNavigator();
+
+// export default function AppNavigator() {
+//   return (
+//     <Stack.Navigator screenOptions={{ headerShown: false }}>
+//       <Stack.Screen name="Splash" component={SplashScreen} />
+//       <Stack.Screen name="Home" component={HomeScreen} />
+//       <Stack.Screen name="PoseDetails" component={PoseDetailScreen}   options={{ headerShown: true, title: 'Pose Details' }} />
+//     </Stack.Navigator>
+//   );
+// }
+
+
+
+
+
+
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from '../screens/SplashScreen';
